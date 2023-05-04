16c2d005067d5d7733ac77d1c0322aa1011f6893
Isaac Libby <ilibbyng@protonmail.com>
2023-05-03 20:29:39 -0400

    added usage

--- basic_structures.py
+++ basic_structures.py
@@ -11,6 +11,11 @@ import sys
 # :print(sys.argv[2])
 
 
+if (len(sys.argv) < 3) or (len(sys.argv) > 3):
+    print ("usage: python basic_structures [-h | -g] [name]")
+    quit() 
+
+
 opt = sys.argv[1]
 person = sys.argv[2]
 
@@ -31,5 +36,7 @@ fruits = ["apple", "banana", "cherry"]
 
 for index, fruit in enumerate(fruits):
     print((index + 1), fruit)
+
+# math
 
 
